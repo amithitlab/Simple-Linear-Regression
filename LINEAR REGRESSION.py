fe586ae8cc7433fc01c1a40653ed8780643df4de
Amit Singh <amit8564880158@gmail.com>
2018-10-11 00:32:38 +0530

    Update LINEAR REGRESSION.py

--- LINEAR REGRESSION.py
+++ LINEAR REGRESSION.py
@@ -5,21 +5,35 @@ import pandas as pd
 np.set_printoptions(suppress=True)
 
 data = pd.read_csv("train.csv")
-
+data = data.dropna()
 x= np.array(data["x"])
+o=x
 y=np.array(data["y"])
+
+
+
 
 x=x.reshape(-1,1)
 one=np.ones((x.shape[0],1))
 x=np.concatenate((one,x),axis=1)
 
 theta=np.array([1.0,1.0])
+alpha=0.001
+
+def cost(x,y,theta):
+    yhat=np.array(x@theta)
+    inner=np.power((yhat-y),2)
+    error= np.nansum(inner)/(2*len(x))
+    return error
 
 
-
-def cost(x,y):
-    inner=np.power(((x@theta)-y),2)
-    return np.nansum(inner)/(2*len(x))
+def gradient_descent(x,y,theta):
+    iter=1000
+    for i in range(iter):
+        theta=theta-(alpha/len(x))*(np.nansum((x@theta-y)*x,axis=0))
+        error2=cost(x,y,theta)
+        if i%100==0:
+            print(error2," ",theta)
     
-l=cost(x,y)
-print(l)
+    
+gradient_descent(x,y,theta)

8574bf51001e22904dd8070bd99c328b117f54e8
Amit Singh <amit8564880158@gmail.com>
2018-10-12 13:17:08 +0530

    Update LINEAR REGRESSION.py

--- LINEAR REGRESSION.py
+++ LINEAR REGRESSION.py
@@ -2,37 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-np.set_printoptions(suppress=True)              //to suppress scientific format
+np.set_printoptions(suppress=True)
 
-data = pd.read_csv("train.csv")                 //reading the dataset
-data = data.dropna()                            //to remove NAN values from the dataset
+data = pd.read_csv("train.csv")
+data = data.dropna()
 x= np.array(data["x"])
 
-y=np.array(data["y"])
-x=x.reshape(-1,1)                               //reshaping the array to vector
+
+y=np.array(data["y"]).reshape(699,1)
+x=x.reshape(-1,1)
 one=np.ones((x.shape[0],1))
 x=np.concatenate((one,x),axis=1)
-o=np.array(x[:,1])                              //concatenating x into one
+o=np.array(x[:,1])
 
-theta=np.array([1.0,1.0])                        //error matrix
-alpha=0.00001                                   //learning rate
 
-def cost(x,y,theta):                            //loss function
+theta=np.random.randn(2,1)*0.001
+print(theta.shape)
+alpha=0.0001
+
+
+def cost(x,y,theta):
     yhat=np.array(x@theta)
+
     inner=np.power((yhat-y),2)
     loss= np.nansum(inner)/(2*len(x))
     return loss
 
 
-def gradient_descent(x,y,theta):                //gradient_descent to minimise loss
+def gradient_descent(x,y,alpha):
+    global theta
     iter=10000
-    for i in range(iter):
-        theta=theta-(alpha/len(x))*(np.nansum((x@theta-y)*o,axis=0))
-        loss2=cost(x,y,theta)
-        if i%100==0:
-            print(loss2," ",theta)              //printing loss
+    t=x.T
+    m=x.shape[0]
+    yhat=np.matmul(x,theta)
+    theta=theta-(alpha/m)*(np.matmul(t,(yhat-y)))
+    return cost(x,y,theta)
+cost2=[]   
+for i in range(10000):
+    cost1=cost(x,y,theta)
+    
+    gradient_descent(x,y,alpha)
+    if i%100==0:
+        print (cost(x,y,theta))
+        cost2.append(cost1)
+   
             
-        
-    
-    
-gradient_descent(x,y,theta)
+a=gradient_descent(x,y,0.001)
+plt.plot(cost2)
+plt.xlabel("Experience")
+plt.ylabel("Salary in K")
+
+
+print(np.matmul(x[:5,:],theta),y[0:5])

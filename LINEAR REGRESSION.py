3ab6ddc061c5452fa895917a2f07689528b11cb0
Amit Singh <amit8564880158@gmail.com>
2018-10-14 19:25:08 +0530

    updated the code with predicting the test values along

--- LINEAR REGRESSION.py
+++ LINEAR REGRESSION.py
@@ -1,56 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 01:17:19 2018
+
+@author: CAPTAIN
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 np.set_printoptions(suppress=True)
 
-data = pd.read_csv("train.csv")
-data = data.dropna()
-x= np.array(data["x"])
-
-
-y=np.array(data["y"]).reshape(699,1)
+data= pd.read_csv("train.csv")
+data=data.dropna()
+x=np.array(data["x"])
+y=np.array(data["y"])
 x=x.reshape(-1,1)
+y=y.reshape(-1,1)
+
+data1=pd.read_csv("test.csv")
+data1=data1.dropna()
+x1=np.array(data1["x"])
+x1=x1.reshape(-1,1)
+y1=np.array(data1["y"])
+y1=y1.reshape(-1,1)
+one1=np.ones((x1.shape[0],1))
+x1=np.hstack((one1,x1))
+
 one=np.ones((x.shape[0],1))
-x=np.concatenate((one,x),axis=1)
-o=np.array(x[:,1])
+x=np.hstack((one,x))
 
-
-theta=np.random.randn(2,1)*0.001
-print(theta.shape)
+theta=np.random.randn(2,1)*0.01
 alpha=0.0001
 
-
-def cost(x,y,theta):
-    yhat=np.array(x@theta)
-
-    inner=np.power((yhat-y),2)
-    loss= np.nansum(inner)/(2*len(x))
-    return loss
-
-
-def gradient_descent(x,y,alpha):
-    global theta
-    iter=10000
-    t=x.T
-    m=x.shape[0]
-    yhat=np.matmul(x,theta)
-    theta=theta-(alpha/m)*(np.matmul(t,(yhat-y)))
-    return cost(x,y,theta)
-cost2=[]   
-for i in range(10000):
-    cost1=cost(x,y,theta)
-    
-    gradient_descent(x,y,alpha)
-    if i%100==0:
-        print (cost(x,y,theta))
-        cost2.append(cost1)
-   
+def grad_descent(x,y,alpha):
+        t=x.T
+        for i in range(10000):
+            global theta,yhat,c2
+            yhat=np.matmul(x,theta)
+            theta=theta- (alpha/x.shape[0])*(np.matmul(t,(yhat-y)))
+            c2=cost(x,y,alpha)
+            if(i%100==0):
+                print(c2)
+        return c2,theta
             
-a=gradient_descent(x,y,0.001)
-plt.plot(cost2)
-plt.xlabel("Experience")
-plt.ylabel("Salary in K")
+            
+def cost(x,y,alpha):
+    yhat=np.matmul(x,theta)
+    sub=np.subtract(yhat,y)
+    c1= (1/(2*x.shape[0]))*(np.sum(np.power(sub,2)))
+    return c1
+
+a,b=grad_descent(x,y,alpha)
+
+ui=np.matmul(x1,b)
 
 
-print(np.matmul(x[:5,:],theta),y[0:5])
+plt.scatter(x1[:,1], y1)
+axes = plt.gca()
+x_vals = np.array(axes.get_xlim()) 
+y_vals = theta[0][0] + theta[1][0]* x_vals #the line equation
+plt.plot(x_vals, y_vals, '--')
+    
+    

10ee59c6713137da690c00e46fbead80251aee70
Amit Singh <amit8564880158@gmail.com>
2018-10-21 22:25:37 +0530

    Update LINEAR REGRESSION.py

--- LINEAR REGRESSION.py
+++ LINEAR REGRESSION.py
@@ -15,30 +15,37 @@ data= pd.read_csv("train.csv")
 data=data.dropna()
 x=np.array(data["x"])
 y=np.array(data["y"])
-x=x.reshape(-1,1)
-y=y.reshape(-1,1)
+theta=np.random.randn(2,1)*0.01
+alpha=0.0001
+
+
 
 data1=pd.read_csv("test.csv")
 data1=data1.dropna()
 x1=np.array(data1["x"])
-x1=x1.reshape(-1,1)
+
 y1=np.array(data1["y"])
-y1=y1.reshape(-1,1)
+
 one1=np.ones((x1.shape[0],1))
-x1=np.hstack((one1,x1))
 
-one=np.ones((x.shape[0],1))
-x=np.hstack((one,x))
 
-theta=np.random.randn(2,1)*0.01
-alpha=0.0001
+def data_preprocess():
+    global x,y,x1,y1
+    x=x.reshape(-1,1)
+    y=y.reshape(-1,1)
+    x1=x1.reshape(-1,1)
+    y1=y1.reshape(-1,1)
+    one=np.ones((x.shape[0],1))
+    x=np.hstack((one,x))
+    x1=np.hstack((one1,x1))
+
 
 def grad_descent(x,y,alpha):
         t=x.T
         for i in range(10000):
             global theta,yhat,c2
             yhat=np.matmul(x,theta)
-            theta=theta- (alpha/x.shape[0])*(np.matmul(t,(yhat-y)))
+            theta=theta-(alpha/x.shape[0])*(np.matmul(t,(yhat-y)))
             c2=cost(x,y,alpha)
             if(i%100==0):
                 print(c2)
@@ -51,15 +58,15 @@ def cost(x,y,alpha):
     c1= (1/(2*x.shape[0]))*(np.sum(np.power(sub,2)))
     return c1
 
+data_preprocess()
 a,b=grad_descent(x,y,alpha)
 
-ui=np.matmul(x1,b)
-
+prediction=np.matmul(x1,b)
 
 plt.scatter(x1[:,1], y1)
 axes = plt.gca()
-x_vals = np.array(axes.get_xlim()) 
+x_vals = np.array(axes.get_xlim())
 y_vals = theta[0][0] + theta[1][0]* x_vals #the line equation
-plt.plot(x_vals, y_vals, '--')
+plt.plot(x_vals,y_vals, '--')
     
     
